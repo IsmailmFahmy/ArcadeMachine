80f697c1c7f4c264592ba0f64595fd6a7772f76c
yyf9991715 <yyf9991715@gmail.com>
2025-01-17 14:53:06 +0100

    challenge 8

--- games/snake/main.py
+++ games/snake/main.py
@@ -4,8 +4,10 @@ Snake Game Implementation
 This script implements the classic Snake game using Pygame. It includes various challenges for functionality,
 such as generating fruit, detecting collisions, and managing game state.
 """
-
+import sys
 import time
+from operator import truediv
+
 import pygame
 import random
 from config import get_game_config, initialize_snake
@@ -39,7 +41,15 @@ def generate_fruit_position(snake_body, window_x, window_y):
     """
     # CHALLENGE 4: make sure the fruit doesn't spawn on the snakes body
     # CHALLENGE 2: Implement the function generate_fruit_position to spawn a fruit RANDOMLY
-    pass
+    fruitx=snake_body[0][0]
+    fruity=snake_body[0][1]
+
+    for [fruit_x, fruit_y] in snake_body:
+        fruitx=random.randint(0,int(window_x/10))*10
+        fruity=random.randint(0,int(window_y/10))*10
+
+    return fruitx, fruity
+
 
 
 def check_boundary_collision(snake_position, window_x, window_y):
@@ -55,7 +65,12 @@ def check_boundary_collision(snake_position, window_x, window_y):
         bool: True if a collision occurred, False otherwise.
     """
     # CHALLENGE 5: stay Inside the Arena!!
+    if snake_position[0] < 0 or snake_position[0] >= window_x or snake_position[1] < 0 or snake_position[1] >= window_y:
+        return True
+    else:
+        return False
     pass
+
 
 
 def check_self_collision(snake_position, snake_body):
@@ -70,10 +85,14 @@ def check_self_collision(snake_position, snake_body):
         bool: True if a self-collision occurred, False otherwise.
     """
     # CHALLENGE 6: Stay in One Piece!!
+    if snake_position in snake_body[1:]:
+        return True
+    else :
+        return False
     pass
 
 
-def game_over():
+def game_over(game_window,window_x,window_y,colors,score):
     """
     Handles the Game Over sequence, displaying a message and pausing the game before quitting.
 
@@ -87,8 +106,6 @@ def game_over():
         None
     """
     # CHALLENGE 7: Game Over in Style!
-<<<<<<< Updated upstream
-=======
     my_font = pygame.font.SysFont('Arial bold', 80)
     game_window.fill(colors['red'])
     text_surface = my_font.render('Game Over', True, colors["green"])
@@ -118,15 +135,13 @@ def game_over():
 
     # pygame.draw.rect(game_window, colors['red'], (window_x//2, window_y//2, 10, 10))
 
->>>>>>> Stashed changes
 
-    time.sleep(3)  # render game over for 3 sec then quit
 
 # CHALLENGE 8: High-score (file operation)
-
 # CHALLENGE 9: pause the screen
 
 # CHALLENGE 10: Bombs
+
 
 ###########################################################################
 # Setup
@@ -152,6 +167,7 @@ fruit_position = [360, 240]
 fruit_spawn = True
 
 #  CHALLENGE 2: call the function you implemented and spawn the fruit
+fruit_position=generate_fruit_position(snake_body, window_x, window_y)
 
 # Define the initial direction and score
 direction = "RIGHT"  # Snake starts moving to the right
@@ -173,29 +189,47 @@ while True:
         if event.type == pygame.QUIT:
             quit()
         elif event.type == pygame.KEYDOWN:
-
             # CHALLENGE 1: Improve input handling for smoother gameplay
-            pass
+            if event.key == pygame.K_LEFT and direction != "RIGHT":
+                direction = "LEFT"
+            elif event.key == pygame.K_RIGHT and direction != "LEFT":
+                direction = "RIGHT"
+            elif event.key == pygame.K_UP and direction != "DOWN":
+                direction = "UP"
+            elif event.key == pygame.K_DOWN and direction != "UP":
+                direction = "DOWN"
+
 
     # CHALLENGE 1: Movement logic
-    if direction == "RIGHT":
+    if direction == "LEFT":
+        snake_position[0] -= 10
+    elif direction == "RIGHT":
         snake_position[0] += 10
-
+    elif direction == "UP":
+        snake_position[1] -= 10
+    elif direction == "DOWN":
+        snake_position[1] += 10
     # Snake body growing mechanism if fruit and snake collide score
     snake_body.insert(0, list(snake_position))
     if (
         snake_position[0] == fruit_position[0]
         and snake_position[1] == fruit_position[1]
     ):
-        # CHALLENGE 3: (Hints 1 and 2)
+        fruit_position=generate_fruit_position(snake_body, window_x, window_y)
+        score += 1
+        print(score)
         pass
     else:
         snake_body.pop()
 
     # CHALLENGE 3: Generate a new fruit position when the previous one is eaten (Hints 3 and 4)
 
+    if(check_boundary_collision(snake_position, window_x, window_y)):
+        game_over(game_window,window_x,window_y,colors,score)
     # CHALLENGE 5: make sure the 'check_boundary_collision' is used
 
+    if(check_self_collision(snake_position, snake_body)):
+        game_over(game_window,window_x,window_y,colors,score)
     # CHALLENGE 6: call the function you implemented
 
     # CHALLENGE 8: High-score Logic
